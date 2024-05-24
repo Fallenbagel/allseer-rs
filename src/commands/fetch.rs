f13033d2f20ea2de6f1ad09ec6ce3654f2d8bd04
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-05-24 22:26:48 +0500

    feat: fetch command

---
+++ src/commands/fetch.rs
@@ -0,0 +1,94 @@
+use serenity::builder::*;
+use serenity::model::application::{CommandInteraction, CommandOptionType};
+use serenity::prelude::*;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tracing::debug;
+
+use crate::handle_interaction::run as handle_interaction;
+use crate::handle_interaction::InteractionType;
+use crate::utils::handler::HashContext;
+use crate::utils::search_type::SearchType;
+
+pub async fn run(
+    ctx: &Context,
+    interaction: &CommandInteraction,
+    context_map: Arc<Mutex<HashMap<u64, HashContext>>>,
+) -> Result<(), color_eyre::Report> {
+    let issue = interaction
+        .data
+        .options
+        .get(1)
+        .unwrap()
+        .value
+        .as_str()
+        .unwrap()
+        == SearchType::Issues.as_str();
+
+    let pr_number = interaction
+        .data
+        .options
+        .first()
+        .unwrap()
+        .value
+        .as_i64()
+        .unwrap();
+
+    let number: u64 = pr_number.try_into()?;
+
+    let response = format!(
+        "Fetching {} #{}",
+        if issue { "issue" } else { "pull request" },
+        number
+    );
+
+    let data = CreateInteractionResponseMessage::new().content(response);
+
+    let builder = CreateInteractionResponse::Message(data);
+
+    interaction.create_response(&ctx.http, builder).await?;
+
+    let mut context = context_map.lock().await;
+
+    // store whether it's an issue or PR in context so it can be retrieved
+    // during the interaction
+    context.insert(
+        number.try_into().unwrap_or(1),
+        HashContext { is_issue: issue },
+    );
+
+    let context_map = context_map.lock().await;
+
+    if let Some(context) = context_map.get(&number) {
+        debug!("Context for {}: {:?}", number, context);
+
+        let interaction = InteractionType::CommandInteraction(interaction.clone());
+
+        handle_interaction(ctx, interaction, number, context).await?;
+    }
+
+    Ok(())
+}
+
+pub fn register() -> CreateCommand {
+    CreateCommand::new("fetch")
+        .description("Fetch a github issue/pull request")
+        .add_option(
+            CreateCommandOption::new(
+                CommandOptionType::String,
+                "type",
+                "Fetch an issue or pull request",
+            )
+            .add_string_choice(SearchType::Issues.as_str(), SearchType::Issues.as_str())
+            .add_string_choice(
+                SearchType::PullRequests.as_str(),
+                SearchType::PullRequests.as_str(),
+            )
+            .required(true),
+        )
+        .add_option(
+            CreateCommandOption::new(CommandOptionType::Integer, "#", "Issue/Pr number")
+                .required(true),
+        )
+}
